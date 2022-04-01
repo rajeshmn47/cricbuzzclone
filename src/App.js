b6a988a55984670589cb924959a6d9bc9d608feb
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-01 06:56:28 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import img from './images/vision.png'
+import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <div className='home'>
+  <div className='topbar'>cricbuzz</div>
+  <div className='belowtopbar'>
+<p style={{color:'#2971B5',fontWeight:'600'}}>Indian Premier League 2022</p>
+<p style={{opacity:'0.5',fontWeight:'600'}}>All the updates from IPL 2022</p>
+  </div>
+  <div className='matches'>
+Matches
+  </div>
+  <div className='matchescontainer'>
+<div className='match'>
+  <p>7th match Indian Premier League 2022</p>
+  <h1>LSG 211/4 (19.3)</h1>
+  <h1>LSG 211/4 (19.3)</h1>
+  <h2>Lucknow Super giants won by 6 wkts</h2>
+</div>
+  </div>
+  <button className='more'>More Matches</button>
+  <div className='matches'>
+Cricket Videos
+  </div>
+  <div className='videoscontainer'>
+  <div className='video'>
+    <img src={img} alt='' width='80'/>
+    <h2>Abdevilliers Smashed a great knock</h2>
+  </div>
+  <div className='video'>
+    <img src={img} alt='' width='80'/>
+    <h2>Abdevilliers Smashed a great knock</h2>
+  </div>
+  </div>
+  <button className='more'>More Videos</button>
+  <div className='matches'>
+IPL 2022 Stats
+  </div>
+  <div style={{display:'flex',padding:'1vmax'}}>
+    <div style={{width:'20%'}}>
+<h2 style={{opacity:'0.5',fontSize:'16px'}}>Most Runs</h2>
+<h2 style={{opacity:'0.5',fontSize:'16px'}}>Most Wickets</h2>
     </div>
+    <div style={{width:'29%'}}>
+    <h2 style={{fontSize:'16px',color:'#4A90E2'}}>Faf du plessis <span style={{fontSize:'12px',color:"#113D59"}}>92 runs</span></h2>
+<h2 style={{fontSize:'16px',color:"#4A90E2"}}>Wanindhu Hasaranga <span style={{fontSize:'12px',color:"#113D59"}}>5 Wickets</span></h2>
+    </div>
+  </div>
+  <div className='matches'>
+Top Stories
+  </div>
+  <button className='more'>More Stories</button>
+  </div>
+  <Footer/>
+  </>
   );
 }
 
